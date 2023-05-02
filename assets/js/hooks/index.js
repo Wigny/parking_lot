fc6e98d9da2a1d922688e49a4f39f750c5e52110
Wígny Almeida <wignybora@gmail.com>
2023-05-02 14:50:15 +0000

    Delete  hook

--- assets/js/hooks/index.js
+++ assets/js/hooks/index.js
@@ -1,9 +1,7 @@
 import DrawCanvas from "./draw_canvas";
 import InputPatternConstraint from "./input_pattern_constraint";
-import ReadClipboard from "./read_clipboard";
 
 export default {
   DrawCanvas,
   InputPatternConstraint,
-  ReadClipboard
 };
