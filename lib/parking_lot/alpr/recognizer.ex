f60c3d6bc256eb56e38f3af67e77531d6537c9b7
Wígny Almeida <wignybora@gmail.com>
2023-06-02 15:39:16 +0000

    Use UFPR models

--- lib/parking_lot/alpr/recognizer.ex
+++ lib/parking_lot/alpr/recognizer.ex
@@ -1,12 +1,28 @@
 defmodule ParkingLot.ALPR.Recognizer do
   @moduledoc """
-  Based on https://github.com/opencv/opencv_zoo/tree/master/models/text_recognition_crnn by
-  https://github.com/cocoa-xu/evision/pull/143
+  Based on https://web.inf.ufpr.br/vri/publications/layout-independent-alpr/
   """
 
   use GenServer
   import Evision.Constant
-  alias Evision.Zoo.{TextDetection, TextRecognition}
+  alias Evision.{DNN, Zoo}
+
+  @models %{
+    detector: %{
+      config:
+        "http://www.inf.ufpr.br/vri/databases/layout-independent-alpr/data/lp-detection-layout-classification.cfg",
+      model:
+        "http://www.inf.ufpr.br/vri/databases/layout-independent-alpr/data/lp-detection-layout-classification.weights"
+    },
+    recognizer: %{
+      config:
+        "http://www.inf.ufpr.br/vri/databases/layout-independent-alpr/data/lp-recognition.cfg",
+      model:
+        "http://www.inf.ufpr.br/vri/databases/layout-independent-alpr/data/lp-recognition.weights",
+      classes:
+        "http://www.inf.ufpr.br/vri/databases/layout-independent-alpr/data/lp-recognition.names"
+    }
+  }
 
   # Client
 
@@ -14,32 +30,26 @@ defmodule ParkingLot.ALPR.Recognizer do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
-  def infer(%{shape: {height, width, _channels}} = image) do
-    resized_image = Evision.resize(image, {736, 736})
+  def infer(image) do
+    {_classIds, _conf, detections} = detect_text(image)
 
-    {detections, _confidences} = detect_text(resized_image)
-    recognitions = Enum.map(detections, &recognize_text(resized_image, &1))
+    with {x, y, w, h} <- List.first(detections) do
+      area = image[[y..(y + h), x..(x + w)]]
 
-    detections =
-      Enum.map(detections, fn points ->
-        Nx.as_type(
-          Nx.multiply(
-            Evision.Mat.to_nx(Evision.boxPoints(points), Nx.BinaryBackend),
-            Nx.tensor([width / 736, height / 736], backend: Nx.BinaryBackend)
-          ),
-          :s32
-        )
-      end)
-
-    Enum.zip(recognitions, detections)
+      if match?(%Evision.Mat{}, area) do
+        recognize_text(area)
+      else
+        nil
+      end
+    end
   end
 
   def detect_text(image) do
     GenServer.call(__MODULE__, {:detect, image}, :infinity)
   end
 
-  def recognize_text(image, detection) do
-    GenServer.call(__MODULE__, {:recognize, detection, image})
+  def recognize_text(image) do
+    GenServer.call(__MODULE__, {:recognize, image})
   end
 
   # Server
@@ -51,27 +61,86 @@ defmodule ParkingLot.ALPR.Recognizer do
 
   @impl true
   def handle_continue(:init, nil) do
-    opts = [backend: cv_DNN_BACKEND_CUDA(), target: cv_DNN_TARGET_CUDA()]
+    detector = detector_init()
+    recognizer = recognizer_init()
+    charset = charset_init()
 
-    detector = TextDetection.DB.init(:td500_resnet18, opts)
-    recognizer = TextRecognition.CRNN.init(:ch, opts)
-
-    {:noreply, %{detector: detector, recognizer: recognizer}}
+    {:noreply, %{detector: detector, recognizer: recognizer, charset: charset}}
   end
 
   @impl true
   def handle_call({:detect, image}, _from, %{detector: detector} = state) do
-    inference = TextDetection.DB.infer(detector, image)
+    inference = DNN.DetectionModel.detect(detector, image, confThreshold: 0.01)
 
     {:reply, inference, state}
   end
 
   @impl true
-  def handle_call({:recognize, detection, image}, _from, %{recognizer: recognizer} = state) do
-    opts = [to_gray: false, charset: TextRecognition.CRNN.get_charset(:ch)]
+  def handle_call({:recognize, image}, _from, state) do
+    %{recognizer: recognizer, charset: charset} = state
 
-    inference = TextRecognition.CRNN.infer(recognizer, image, detection, opts)
+    blob = DNN.blobFromImage(image, scalefactor: 1 / 255, size: {352, 128}, swapRB: true)
+
+    recognizer = DNN.Net.setInput(recognizer, blob)
+
+    output_layers_names = DNN.Net.getUnconnectedOutLayersNames(recognizer)
+    [outputBlob] = DNN.Net.forward(recognizer, outBlobNames: output_layers_names)
+
+    output = Evision.Mat.to_nx(outputBlob, Nx.BinaryBackend)
+
+    inference =
+      0..(Nx.axis_size(output, 0) - 1)
+      |> Enum.reduce([], fn i, acc ->
+        scores = output[[i, 5..-1//1]]
+
+        class_id = Nx.to_number(Nx.argmax(scores))
+        confidence = Nx.to_number(scores[class_id])
+
+        if confidence > 0.5 do
+          char = Enum.at(charset, class_id)
+          Enum.concat(acc, [char])
+        else
+          acc
+        end
+      end)
+      |> Enum.join()
 
     {:reply, inference, state}
+  end
+
+  defp detector_init() do
+    download_opts = [cache_dir: System.tmp_dir!()]
+
+    {:ok, config} = Zoo.download(@models.detector.config, "detection.cfg", download_opts)
+    {:ok, model} = Zoo.download(@models.detector.model, "detection.weights", download_opts)
+
+    net = DNN.readNetFromDarknet(config, darknetModel: model)
+    DNN.Net.setPreferableTarget(net, cv_DNN_TARGET_CPU())
+
+    model = DNN.DetectionModel.detectionModel(net)
+
+    DNN.DetectionModel.setInputParams(model, scale: 1 / 255, size: {416, 416}, swapRB: true)
+
+    model
+  end
+
+  defp recognizer_init() do
+    download_opts = [cache_dir: System.tmp_dir!()]
+
+    {:ok, config} = Zoo.download(@models.recognizer.config, "recognition.cfg", download_opts)
+    {:ok, model} = Zoo.download(@models.recognizer.model, "recognition.weights", download_opts)
+
+    net = DNN.readNetFromDarknet(config, darknetModel: model)
+    DNN.Net.setPreferableTarget(net, cv_DNN_TARGET_CPU())
+
+    net
+  end
+
+  defp charset_init() do
+    download_opts = [cache_dir: System.tmp_dir!()]
+
+    {:ok, names} = Zoo.download(@models.recognizer.classes, "recognition.names", download_opts)
+
+    String.split(File.read!(names), "\n")
   end
 end
---
+++ test/parking_lot/alpr/recognizer_test.exs
@@ -0,0 +1,17 @@
+defmodule ParkingLot.ALPR.Text.RecognizerTest do
+  use ParkingLot.DataCase
+
+  alias ParkingLot.ALPR.Recognizer
+
+  @license_plate "test/support/files/plate.png"
+
+  describe "infer/1" do
+    setup do
+      %{image: Evision.imread(@license_plate)}
+    end
+
+    test "recognizes plate digits in image", %{image: image} do
+      assert "RSW6A87" = Recognizer.infer(image)
+    end
+  end
+end
