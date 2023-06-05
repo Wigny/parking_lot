f8478eea0076469c5816db8a67c709585377d3cc
Wígny Almeida <wignybora@gmail.com>
2023-06-05 00:55:01 +0000

    Fix test

--- test/parking_lot/algorithm_test.exs
+++ test/parking_lot/algorithm_test.exs
@@ -5,7 +5,12 @@ defmodule ParkingLot.AlgorithmTest do
 
   describe "majority_voting/1" do
     test "returns the most frequently predicted character at each position" do
-      assert "RSW6A87" == Algorithm.majority_voting(["RSW6A87", "RSW6487", "RSWGA87"])
+      assert ~w"R S W 6 A 8 7" ==
+               Algorithm.majority_voting([
+                 ~w"R S W 6 A 8 7",
+                 ~w"R S W 6 4 8 7",
+                 ~w"R S W G A 8 7"
+               ])
     end
   end
 end
