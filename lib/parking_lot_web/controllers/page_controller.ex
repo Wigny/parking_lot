20389d6b85fd673f1386209c1b1b66d90af39c70
Wígny Almeida <wignybora@gmail.com>
2023-08-04 05:02:31 +0000

    Add pages

--- lib/parking_lot_web/controllers/page_controller.ex
+++ lib/parking_lot_web/controllers/page_controller.ex
@@ -1,9 +1,9 @@
 defmodule ParkingLotWeb.PageController do
   use ParkingLotWeb, :controller
 
-  def home(conn, _params) do
+  def index(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :index, layout: false)
   end
 end
